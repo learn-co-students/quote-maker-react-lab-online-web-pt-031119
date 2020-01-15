9ff652d876281db4ff3a749c0782c3c578c3652f
lieba <liebahinda@gmail.com>
2020-01-14 19:23:39 -0500

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,31 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+
+
+
+
+
+
 
 const QuoteCard = (props) =>
+
+
+
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
+        <textarea name="test" content="test quote" />
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <textarea name="quote test" content="testing this quote" />
+          <textarea name="author test" content="test author" />
+          <textarea name="author test2" content="authoring this test" />
+
+
+
+
+          <p>{props.quote.content}</p>
+          
+          <footer> Author: <cite title="Source Title">{props.quote.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
@@ -14,25 +33,32 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
+            onClick={() => props.upvoteQuote(props.quote.id)}
           >
-            Upvote
+            Upvote 
           </button>
           <button
             type="button"
             className="btn btn-secondary"
+            onClick={() => props.downvoteQuote(props.quote.id)}
           >
             Downvote
           </button>
           <button
             type="button"
             className="btn btn-danger"
+
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+
+        <div className="vote count" value={props.quote.votes}>Votes: {props.quote.votes}</div>
+
       </div>
     </div>
   </div>;
+
+
 
 export default QuoteCard;
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,59 @@
 export default (state = [], action) => {
-  return state;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+
+      return [
+        ...state, {
+          author: action.quote.author,
+          content: action.quote.content,
+          id: action.quote.id,
+          votes: action.quote.votes
+        }
+      ];
+
+    case 'REMOVE_QUOTE':
+      let index = state.filter(quote => quote.id !== action.quoteId);
+
+        return index
+
+
+    case 'UPVOTE_QUOTE':
+      return state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return {
+            ...quote,
+            // author: quote.author,
+            // content: quote.content,
+            // id: quote.id,
+            votes: quote.votes += 1
+          }
+        } else {
+            return quote
+        }
+      }
+  )
+
+
+
+  case 'DOWNVOTE_QUOTE':
+    return state.map(quote => {
+      if (quote.id === action.quoteId && quote.votes > 0) {
+        return {
+          ...quote,
+          // author: quote.author,
+          // content: quote.content,
+          // id: quote.id,
+          votes: quote.votes -= 1
+        }
+      } else {
+          return quote
+      }
+    }
+  )
+
+  default:
+    return state;
+  }
+
 }
